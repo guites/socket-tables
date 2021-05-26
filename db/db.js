ef5622ad4eee19a2eb32d05a7c22b11ab69e1b77
Guilherme <guilherme@astrusweb.com>
2021-05-26 20:13:38 -0300

    manter ordem id asc após adc novo inner join

--- db/db.js
+++ db/db.js
@@ -26,7 +26,8 @@ async function getAllAtendimentos() {
     atd.obs
     FROM atendimentos atd
     INNER JOIN clientes c ON c.sort_id = atd.client_id
-    INNER JOIN status s ON atd.status_id = s.id`
+    INNER JOIN status s ON atd.status_id = s.id
+    ORDER BY atd.id ASC`
   );
   return atendimentos;
 }
